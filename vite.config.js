aceb0cb5a65f2ede754e6b981b895b87ce1b2db2
E-Was24 <satriaelan5@gmail.com>
2025-11-08 18:33:06 +0700

    update vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // Sesuaikan nama repo
 export default defineConfig({
-  base: '/Bot_Master/', 
+  base: process.env.NODE_ENV === 'production' ? '/Bot_Master/' : '/',
   plugins: [react()],
 })
